d386b03e7ed7c5fdaa9d0d8d5a8008a7c7b1e877
GregerGit <carlgregorys96@gmail.com>
2026-09-20 12:59:28 +0200

    Tier values
    Fixed tier value on T4 resources

--- Scripts/ResourceDefinition.cs
+++ Scripts/ResourceDefinition.cs
@@ -5,7 +5,7 @@ public partial class ResourceDefinition : Resource
 {
 	[Export] public string DisplayName = "Unnamed";
 
-	[Export(PropertyHint.Range, "1, 3, 1")]
+	[Export(PropertyHint.Range, "1, 5, 1")]
 	public int Tier = 1;
 
 	[Export] public int SellValue = 1;
